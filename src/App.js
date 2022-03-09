da43eef6569c392f7c66572eff6bf5cd94c449fb
Simón A Azócar <saimn.azocar@gmail.com>
2022-03-08 21:10:49 -0400

    added basic structure on react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 
-function App() {
+
+const todos = [
+  {
+    text: 'Ver curso',
+    completed: false,
+  },
+  {
+    text: 'Tomar café',
+    completed: false,
+  },
+  {
+    text: 'Ir al gym',
+    completed: false,
+  },
+  {
+    text: 'Ver Peaky Blinders',
+    completed: false,
+  },
+]
+
+const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList >
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList >
+      <CreateTodoButton /> 
+    </React.Fragment>
   );
 }
 
---
+++ src/TodoSearch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TodoSearch = () =>{
+    return (
+        <input placeholder="What do yo have to do?" />
+    );
+};
+
+export { TodoSearch };
