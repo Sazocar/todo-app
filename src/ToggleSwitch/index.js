54a34abec568cba174cc68274db911bf950210ba
Simón A Azócar <saimn.azocar@gmail.com>
2022-03-27 17:57:08 -0400

    changes on icons an position on mobile

--- src/ToggleSwitch/index.js
+++ src/ToggleSwitch/index.js
@@ -3,8 +3,8 @@ import "./ToggleSwitch.css";
   
 const ToggleSwitch =({ theme, setTheme }) => {
 
-	const lightMoon = 'https://img.icons8.com/ios-filled/32/000000/do-not-disturb-2.png';
-	const darkMoon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAABgElEQVRIidWWS07DMBCGJxWIdgErWonSFeImvQEqx4A78LgH3KEcAEjLY9GC0hvAjhUtSDwW/VjElZIosR07UPFL3iTzzzeeKPaI/DcBd8DNMsAAuPprVRbjDQa2gYZvcmAVaNsGt4FPYAK0NHEhcK153wIi4APYsgHXlQET3ACdqBwRsOZiLNyVxh+aCs/9xkEQvIhIV0SGIvKcKegUeADe1RoDJxnAk/J2VS53AfvAjGLNgJ4XpAA610AXmlcGV+3V7TSrKdA05bU5QA5FZL1ErRsiclAiPl/AY4ndLjQ25a2p5GHGGCZidhzq3U0Unpu7qNXOh7+F3y438T/7O6026MKqwrT6Dp60KP87vQKb3mAF72F/gOxVAs3Ap4ad+kNVi4fAWeJZEzgGRsCbWiPgKNle4BwYUPZKJX0tXjoUfaW89vc50EhAqxoEzGMU8bz1rQy60eceuDXAI+CLEnNXB6gbYgD9eEs8RnWsoLayAeu0tLl6xcM7EP/L5O/1AzM0BnlHGDx/AAAAAElFTkSuQmCC';
+	const moon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAABbUlEQVRIie2Wvy8lYRRAz6yVrIQVhMSvbqNQKKlINmEbyZPtthGFRqPUq2yi0aj8BwrRakn0Ovs6dsVrtrWbII5i/HgzxrzxfO9F4ZQ3d+bce/PdmQ/eeWuoA80WjquHxpSaJV1Rr3zkazOkP01yon5otHTZp6w3Wjqq/s8QL4b0ZI1uDfiUEf/dMLE6AswVyQ0qBn7k5Abd47R4Mid3IqQ4gVrJOFT3nKstoVzpjjtzcvuB+VDiBOq/nI5V/6hdIVzpjk9r5A8B2+rH0OJygWdmgF3182vlD6hLNUZdTdkCfyu1TW1Px6NUUg9QAVpfUO8RsAPsA2fABTAIfAG+A6W72HAURZd5FW6+oOuiHKvRs9I7cbf6N6D0Rp0uNDv1m3oZSLxWSFolXzB5+6iHLeu5PKhT5n9Gn+NaXa1LWiXvVTfMvhykuVH31LFa780/ackC+ohXYxYYIV6ZDuL1KwMHwHYURb/q6vCdRnELPwnPSLAWcJ8AAAAASUVORK5CYII=';
+	const sun = 'https://img.icons8.com/ios-glyphs/30/000000/sun--v1.png';
 	
 	const themeToggler = () => {
 		theme == 'light' ? setTheme('dark') : setTheme('light')
@@ -14,7 +14,7 @@ const ToggleSwitch =({ theme, setTheme }) => {
 		<React.Fragment>
 			<img 
 				className="themeMode-icon"
-				src={theme == 'light' ? lightMoon : darkMoon}
+				src={theme == 'light' ? sun : moon}
 			/>
 			<label className="switch">
 				<input type="checkbox"
