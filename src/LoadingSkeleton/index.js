4065795615850ca74d93e1b9acb80961c86ed2c7
Simón A Azócar <saimn.azocar@gmail.com>
2022-03-27 22:30:36 -0400

    delete thrash can on loading skeleton

--- src/LoadingSkeleton/index.js
+++ src/LoadingSkeleton/index.js
@@ -13,8 +13,8 @@ const LoadingSkeleton = (props) => (
     {...props}
   >
     <circle cx="28" cy="25" r="15" />
-    <rect x="56" y="14" rx="5" ry="5" width="381" height="22" /> 
-    <rect x="470" y="12" rx="5" ry="5" width="22" height="25" /> 
+    <rect x="56" y="14" rx="5" ry="5" width="435" height="22" /> 
+    {/* <rect x="470" y="12" rx="5" ry="5" width="22" height="25" />  */}
   </ContentLoader>
 )
 
