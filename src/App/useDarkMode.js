5ad8f1af64908749888d388c95d4a40cbe5600a1
Simón A Azócar <saimn.azocar@gmail.com>
2023-02-28 20:15:31 -0400

    ADD react-hot-toast library in useDarkMode

--- src/App/useDarkMode.js
+++ src/App/useDarkMode.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React from 'react'
+import toast from 'react-hot-toast'
 
 const useDarkMode = () => {
-  const [theme, setTheme] = React.useState("light");
+  const [theme, setTheme] = React.useState('light')
 
   const setMode = (mode) => {
-    localStorage.setItem("theme", mode);
-    setTheme(mode);
-  };
+    localStorage.setItem('theme', mode)
+    setTheme(mode)
+  }
 
   const themeToggler = () => {
-    theme === "light" ? setMode("dark") : setMode("light");
-  };
+    if (theme === 'light') {
+      setMode('dark')
+      toast('Hello Darkness!', {
+        icon: '👏',
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+          fontFamily: 'Quattrocento Sans',
+        },
+      })
+    } else if (theme === 'dark') {
+      setMode('light')
+      toast('Hello Brightness!', {
+        icon: '👏',
+        style: {
+          borderRadius: '10px',
+          background: '#fff',
+          color: '#333',
+          fontFamily: 'Quattrocento Sans',
+        },
+      })
+    }
+  }
 
   React.useEffect(() => {
-    const localTheme = localStorage.getItem("theme");
-    localTheme && setTheme(localTheme);
-  }, []);
+    const localTheme = localStorage.getItem('theme')
+    localTheme && setTheme(localTheme)
+  }, [])
 
-  return [theme, themeToggler];
-};
+  return [theme, themeToggler]
+}
 
-export { useDarkMode };
+export { useDarkMode }
+
--- src/ToggleSwitch/index.js
+++ src/ToggleSwitch/index.js
@@ -1,19 +1,24 @@
-import React from "react";
-import "./ToggleSwitch.css";
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import './ToggleSwitch.css'
 
 const ToggleSwitch = ({ theme, onToggle }) => {
-  const moon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAABbUlEQVRIie2Wvy8lYRRAz6yVrIQVhMSvbqNQKKlINmEbyZPtthGFRqPUq2yi0aj8BwrRakn0Ovs6dsVrtrWbII5i/HgzxrzxfO9F4ZQ3d+bce/PdmQ/eeWuoA80WjquHxpSaJV1Rr3zkazOkP01yon5otHTZp6w3Wjqq/s8QL4b0ZI1uDfiUEf/dMLE6AswVyQ0qBn7k5Abd47R4Mid3IqQ4gVrJOFT3nKstoVzpjjtzcvuB+VDiBOq/nI5V/6hdIVzpjk9r5A8B2+rH0OJygWdmgF3182vlD6hLNUZdTdkCfyu1TW1Px6NUUg9QAVpfUO8RsAPsA2fABTAIfAG+A6W72HAURZd5FW6+oOuiHKvRs9I7cbf6N6D0Rp0uNDv1m3oZSLxWSFolXzB5+6iHLeu5PKhT5n9Gn+NaXa1LWiXvVTfMvhykuVH31LFa780/ackC+ohXYxYYIV6ZDuL1KwMHwHYURb/q6vCdRnELPwnPSLAWcJ8AAAAASUVORK5CYII=";
-  const sun = "https://img.icons8.com/ios-glyphs/30/000000/sun--v1.png";
+  const moon =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAABbUlEQVRIie2Wvy8lYRRAz6yVrIQVhMSvbqNQKKlINmEbyZPtthGFRqPUq2yi0aj8BwrRakn0Ovs6dsVrtrWbII5i/HgzxrzxfO9F4ZQ3d+bce/PdmQ/eeWuoA80WjquHxpSaJV1Rr3zkazOkP01yon5otHTZp6w3Wjqq/s8QL4b0ZI1uDfiUEf/dMLE6AswVyQ0qBn7k5Abd47R4Mid3IqQ4gVrJOFT3nKstoVzpjjtzcvuB+VDiBOq/nI5V/6hdIVzpjk9r5A8B2+rH0OJygWdmgF3182vlD6hLNUZdTdkCfyu1TW1Px6NUUg9QAVpfUO8RsAPsA2fABTAIfAG+A6W72HAURZd5FW6+oOuiHKvRs9I7cbf6N6D0Rp0uNDv1m3oZSLxWSFolXzB5+6iHLeu5PKhT5n9Gn+NaXa1LWiXvVTfMvhykuVH31LFa780/ackC+ohXYxYYIV6ZDuL1KwMHwHYURb/q6vCdRnELPwnPSLAWcJ8AAAAASUVORK5CYII='
+  const sun = 'https://img.icons8.com/ios-glyphs/30/000000/sun--v1.png'
 
   return (
     <React.Fragment>
-      <img className="themeMode-icon" src={theme == "light" ? sun : moon} />
-      <label className="switch">
-        <input type="checkbox" onClick={onToggle} />
-        <span className="slider round"></span>
+      <img className='themeMode-icon' src={theme == 'light' ? sun : moon} />
+      <label className='switch'>
+        <input type='checkbox' onClick={onToggle} />
+        <span className='slider round'></span>
       </label>
-    </React.Fragment>
-  );
-};
 
-export default ToggleSwitch;
+      <Toaster position='bottom-center'/>
+    </React.Fragment>
+  )
+}
+
+export default ToggleSwitch
+
